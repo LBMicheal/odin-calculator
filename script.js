d2a2114ebd46a047881fe0ae700cd3f55c251cad
LBMicheal <bohlmeyerm@gmail.com>
2023-03-13 00:06:28 -0500

    add operate function main function to operate math functions

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 // VARIABLES
-let numbers = [];
+let number1 = 8;
+let number2 = 4;
+let operator;
 let display = 0;
 
-function add(array) {
-  return array.reduce((total, number) => (total += number));
+// MATH FUNCTIONS
+function add(number1, number2) {
+  return number1 + number2;
 }
 
-function subtract(array) {
-  return array.reduce((total, number) => (total -= number));
+function subtract(number1, number2) {
+  return number1 - number2;
 }
 
-function multiply(array) {
-  return array.reduce((total, number) => (total *= number));
+function multiply(number1, number2) {
+  return number1 * number2;
 }
 
-function divide(array) {
-  return array.reduce((total, number) => (total /= number));
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// function for running the math functions
+function operate(operator, number1, number2) {
+  switch (operator) {
+    case add:
+      return add(number1, number2);
+      break;
+    case subtract:
+      return subtract(number1, number2);
+      break;
+    case multiply:
+      return multiply(number1, number2);
+      break;
+    case divide:
+      return divide(number1, number2);
+      break;
+    default:
+      return "OOPS";
+  }
 }
